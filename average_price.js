75114c6550e1538a527881e9d61cf3e7b14e5518
ltrias <lucas.trias@gmail.com>
2023-04-11 14:32:28 -0300

    Adding unit tests

---
+++ average_price.js
@@ -0,0 +1,107 @@
+class AveragePrice {
+
+  constructor(assetName){
+    if( !assetName ){
+      throw "Invalid asset name"
+    }
+
+    this.assetName = assetName
+    this.totalAmount = 0;
+    this.totalValue = 0
+    this.avgPrice = 0
+    this.optAvgPrice = 0
+    this.operationLog = new Map()
+  }
+
+  /*
+  Casos de opções:
+    - negociação ( venda e compra mesma opção)
+    - opções com mais de uma operação( calcular PM da opção?)
+    - negociações parciais, venda < que o total de ativos possuído
+
+
+    - venda coberta de call com exercício -> gera venda, PM zera 
+    - venda coberta de call sem exercício -> muda PM sintético. Planilha não traz valor abatido. Marcado com E no final, nao aparece ativo
+    - venda coberta de put com exercício -> gera compra, abate valor prêmio da opção do PM do ativo. Planilha não traz valor abatido. Marcado com E no final, nao aparece ativo
+    - venda coberta de put sem exercício -> muda PM sintético
+    - compra coberta de call com exercício -> nao tratar
+    - compra coberta de call sem exercício -> nao tratar
+    - compra coberta de put com exercício -> nao tratar
+    - compra coberta de put sem exercício -> nao tratar
+  */
+  addOperation(date, operationType, assetCode, amount, unitPrice){
+    if( (amount <= 0 && operationType == "Compra") || (amount >= 0 && operationType == "Venda") ){
+      throw "Operação e/ou quantidade inválida"
+    }
+
+    this.operationLog.set(assetCode, {arguments})
+
+    if(assetCode.startsWith("EZTC")) {
+      console.log(arguments)
+    }
+    switch(operationType){
+      case 'Compra':
+        if( this.firstBuy() ){
+          this.totalAmount = amount
+          this.totalValue = unitPrice * amount
+          this.avgPrice = this.totalValue / this.totalAmount
+          this.optAvgPrice += this.avgPrice 
+        }
+        else{
+          this.totalAmount += amount
+          this.totalValue += unitPrice * amount
+          
+          this.avgPrice = this.totalValue / this.totalAmount
+          this.optAvgPrice = this.avgPrice
+        }
+        break;
+      case 'Venda':
+        if( this.isCall(assetCode) ){
+          // this.optAvgPrice -= unitPrice
+          // throw "PArei aqui"
+          console.log("Venda de call: " + assetCode)
+        } else if( this.isPut(assetCode) ){
+          // console.log("Venda de Put: " + assetCode)
+          // this.totalAmount += amount
+          // this.totalValue += unitPrice * amount
+          
+          // this.avgPrice = this.totalValue / this.totalAmount
+          this.optAvgPrice -= unitPrice
+        }
+        else{
+          this.totalAmount += amount
+          this.totalValue = this.totalValue / this.totalAmount
+
+          if ( this.totalAmount == 0 ){
+            this.totalValue = 0
+            this.avgPrice = 0
+            this.optAvgPrice = 0
+          }else{
+            this.totalValue = this.avgPrice * this.totalAmount
+          }
+        }
+        break;
+      default:
+        throw "Unknown Operation: " + type
+    }
+  }
+
+
+  isCall(assetCode){
+    return assetCode.charCodeAt(4) && assetCode.charCodeAt(4) >= 65 && assetCode.charCodeAt(4) <= 76
+  }
+
+  isPut(assetCode){
+    return assetCode.charCodeAt(4) && assetCode.charCodeAt(4) >= 77 && assetCode.charCodeAt(4) < 90
+  }
+
+  hasBeenExercised(assetCode){
+    return assetCode.endsWith('E')
+  }
+
+  firstBuy(){
+    return this.totalAmount == 0
+  }
+}
+
+module.exports = AveragePrice
---
+++ average_price.test.js
@@ -0,0 +1,191 @@
+const AveragePrice = require('./average_price');
+
+describe('Average Price construction', () =>{
+    test('AP should know its asset name', () => {
+        expect(new AveragePrice("ABCD").assetName).toBe("ABCD");
+    });
+    
+    test('Eventless average price should be 0', () => {
+        expect(new AveragePrice("ABCD").avgPrice).toBe(0);
+    });
+    
+    test('Eventless synthetic average price should be 0', () => {
+        expect(new AveragePrice("ABCD").optAvgPrice).toBe(0);
+    });
+    
+    test('Eventless average price should not have amount', () => {
+        expect(new AveragePrice("ABCD").totalAmount).toBe(0);
+    });
+    
+    test('Eventless average price should not have total value', () => {
+        expect(new AveragePrice("ABCD").totalValue).toBe(0);
+    });
+})
+
+describe('No derivative operations', () =>{
+    test('Validate single buy', () => {
+        var ap = new AveragePrice("ABCD")
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        expect(ap.avgPrice).toBe(15.00);
+        expect(ap.optAvgPrice).toBe(15.00);
+        expect(ap.totalAmount).toBe(1000);
+        expect(ap.totalValue).toBe(15000);
+    });
+    
+    test('Total sell should zero all values', () => {
+        var ap = new AveragePrice("ABCD")
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        ap.addOperation(Date.parse('02 Jan 1970 00:00:00 GMT'), "Venda", "ABCD", -1000, 0.00)
+        expect(ap.totalAmount).toBe(0);
+        expect(ap.totalValue).toBe(0);
+        expect(ap.avgPrice).toBe(0);
+        expect(ap.optAvgPrice).toBe(0);
+    });
+
+    test('Many buys should should update average price', () => {
+        var ap = new AveragePrice("ABCD")
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 100, 10.00)
+        ap.addOperation(Date.parse('02 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 100, 11.00)
+        expect(ap.totalAmount).toBe(200);
+        expect(ap.totalValue).toBe(2100);
+        expect(ap.avgPrice).toBe(10.50);
+        expect(ap.optAvgPrice).toBe(10.50);
+    });
+
+    test('Partial sell should keep average price and update total value', () => {
+        var ap = new AveragePrice("ABCD")
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 200, 10.50)
+        ap.addOperation(Date.parse('02 Jan 1970 00:00:00 GMT'), "Venda", "ABCD", -50, 12.00)
+        expect(ap.totalAmount).toBe(150);
+        expect(ap.totalValue).toBe(1575);
+        expect(ap.avgPrice).toBe(10.50);
+        expect(ap.optAvgPrice).toBe(10.50);
+    });
+
+    test('New buys after total sell should recalculate all values', () => {
+        var ap = new AveragePrice("ABCD")
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Venda", "ABCD", -1000, 0.00)
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 200, 10.50)
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Venda", "ABCD", -50, 12.00)
+        expect(ap.totalAmount).toBe(150);
+        expect(ap.totalValue).toBe(1575);
+        expect(ap.avgPrice).toBe(10.50);
+        expect(ap.optAvgPrice).toBe(10.50);
+    });
+})
+
+describe('Put Operations', () => {
+    test('Valid assset codes', () => {
+        var ap = new AveragePrice("ABCD")
+
+        expect(ap.isPut('')).toBeFalsy()
+        expect(ap.isPut('ABCD')).toBeFalsy()
+        expect(ap.isPut('ABCD4')).toBeFalsy()
+        expect(ap.isPut('ABCDA123')).toBeFalsy()
+        expect(ap.isPut('ABCDL123')).toBeFalsy()
+        expect(ap.isPut('ABCDM123')).toBeTruthy()
+        expect(ap.isPut('ABCDR123')).toBeTruthy()
+        expect(ap.isPut('ABCDX123')).toBeTruthy()
+        expect(ap.isPut('ABCDZ123')).toBeFalsy()
+    });
+
+    test('Single buy with put exercise both average prices should be changed and equal ', () => {
+        var ap = new AveragePrice("EZTC")
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Venda", "EZTCX160", -1000, 0.7168)
+        console.log(ap)
+        ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "EZTCX160E", 1000, 15.8055)
+        expect(ap.avgPrice).toBe(15.8055)
+        expect(ap.optAvgPrice).toBe(15.0886)
+        expect(ap.totalAmount).toBe(1000)
+        expect(ap.totalValue).toBe(15088.60)
+    });
+
+    test.skip('Second buy with put exercise should', () => {
+        var ap = new AveragePrice("ABCD")
+        // ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        // expect(ap.avgPrice).toBe(15.00);
+        // expect(ap.optAvgPrice).toBe(15.00);
+        // expect(ap.totalAmount).toBe(1000);
+        // expect(ap.totalValue).toBe(15000);
+    });
+
+    test.skip('Non exercised put should change only synth average prices', () => {
+        var ap = new AveragePrice("ABCD")
+        // ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        // expect(ap.avgPrice).toBe(15.00);
+        // expect(ap.optAvgPrice).toBe(15.00);
+        // expect(ap.totalAmount).toBe(1000);
+        // expect(ap.totalValue).toBe(15000);
+    });
+})
+
+
+describe('Call Operations', () =>{
+    test('Valid assset codes', () => {
+        var ap = new AveragePrice("ABCD")
+
+        expect(ap.isCall('')).toBeFalsy()
+        expect(ap.isCall('ABCD')).toBeFalsy()
+        expect(ap.isCall('ABCD4')).toBeFalsy()
+        expect(ap.isCall('ABCDA123')).toBeTruthy()
+        expect(ap.isCall('ABCDH123')).toBeTruthy()
+        expect(ap.isCall('ABCDL123')).toBeTruthy()
+        expect(ap.isCall('ABCDM123')).toBeFalsy()
+        expect(ap.isCall('ABCDX123')).toBeFalsy()
+    });
+
+    test.skip('', () => {
+        var ap = new AveragePrice("ABCD")
+        // ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        // expect(ap.avgPrice).toBe(15.00);
+        // expect(ap.optAvgPrice).toBe(15.00);
+        // expect(ap.totalAmount).toBe(1000);
+        // expect(ap.totalValue).toBe(15000);
+    });
+})
+
+
+
+describe('Call and Put Mixed Operations', () =>{
+    test('Valid assset codes', () => {
+        var ap = new AveragePrice("ABCD")
+
+        expect(ap.hasBeenExercised('')).toBeFalsy()
+        expect(ap.hasBeenExercised('ABCD')).toBeFalsy()
+        expect(ap.hasBeenExercised('ABCD4')).toBeFalsy()
+        expect(ap.hasBeenExercised('ABCDA123')).toBeFalsy()
+        expect(ap.hasBeenExercised('ABCDA123E')).toBeTruthy()
+        expect(ap.hasBeenExercised('ABCDM123')).toBeFalsy()
+        expect(ap.hasBeenExercised('ABCDM123E')).toBeTruthy()
+        expect(ap.hasBeenExercised('ABCDX123')).toBeFalsy()
+    });
+
+    test('Invalid operation and amounts should throw error', () => {
+        function buy_wrapper(){
+            new AveragePrice("ABCD").addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", -1000, 15.00)
+        }
+
+        function sell_wrapper(){
+            new AveragePrice("ABCD").addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Venda", "ABCD", 1000, 15.00)
+        }
+
+        expect(buy_wrapper).toThrow()
+        expect(sell_wrapper).toThrow()
+    })
+
+    test.skip('', () => {
+        var ap = new AveragePrice("ABCD")
+        // ap.addOperation(Date.parse('01 Jan 1970 00:00:00 GMT'), "Compra", "ABCD", 1000, 15.00)
+        // expect(ap.avgPrice).toBe(15.00);
+        // expect(ap.optAvgPrice).toBe(15.00);
+        // expect(ap.totalAmount).toBe(1000);
+        // expect(ap.totalValue).toBe(15000);
+    });
+})
+
+
+
+test('', () => {
+    expect(new AveragePrice("ABCD").avgPrice).toBe(0);
+})
